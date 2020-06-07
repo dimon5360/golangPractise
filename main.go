60d9cfc2088fb85d8596d9936b200a86671a6416
dimon5360 <dimon5360@gmail.com>
2020-06-07 20:51:34 +0300

    First build v.0.0.1.

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// BUILD : Current version of build
+const BUILD uint32 = 1
+
+// MINOR : Minor version of application
+const MINOR uint32 = 0
+
+// MAJOR : Major version of application
+const MAJOR uint32 = 0
+
 func main() {
-	fmt.Println("Hello, World\n")
+
+	fmt.Printf("Application version %d.%d.%d.\n", MAJOR, MINOR, BUILD)
+	fmt.Println("Start application")
 }

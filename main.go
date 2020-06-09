f5febcbbd24a968446e8d4bb30e7f12e1ec00a04
dimon5360 <dimon5360@gmail.com>
2020-06-09 23:02:46 +0300

    TCP server realized. Build v.0.0.3 from 09.06.2020.

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
 
 // BUILD : Current version of build
-const BUILD uint32 = 1
+const BUILD uint32 = 2
 
 // MINOR : Minor version of application
 const MINOR uint32 = 0
@@ -13,6 +19,37 @@ const MAJOR uint32 = 0
 
 func main() {
 
+	timestamp := time.Now()
 	fmt.Printf("Application version %d.%d.%d.\n", MAJOR, MINOR, BUILD)
-	fmt.Println("Start application")
+	fmt.Println("Time of build: ", timestamp)
+	fmt.Println("Start application.")
+
+	StartTCPServer()
+
+}
+
+// StartTCPServer : Start TCP server on defined port 40400
+func StartTCPServer() {
+	fmt.Println("Start of TCP server")
+
+	var port string = ":40400"
+
+	// start listening the port
+	ln, _ := net.Listen("tcp", port)
+
+	// wait accept
+	conn, _ := ln.Accept()
+
+	for {
+		// wait new message
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+
+		fmt.Print("Message received: ", string(message))
+
+		// prepare the response
+		newMessage := strings.ToUpper(message)
+
+		//send the response
+		conn.Write([]byte(newMessage + "\n"))
+	}
 }
